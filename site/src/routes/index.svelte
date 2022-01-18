726a60ffdf1164b29a8973ea2bb311013554806b
Riley Stewart <me@rileystew.art>
2022-01-18 15:52:06 -0800

    site verbiage changes

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -7,9 +7,11 @@
   .landing-point {
     margin-bottom: 2rem;
   }
-  .landing-container {
-    width: 30rem;
-    padding: 5rem;
+  @media(min-width: 480px) {
+    .landing-container {
+      width: 30rem;
+      padding: 5rem;
+    }
   }
 </style>
 
@@ -19,9 +21,8 @@
     <div class="landing-point">Various chaotic phenomena emerge out of the collective behavior of these agents, despite each agent having relatively simple behavior.</div>
     <div class="landing-point">These phenomena are difficult to describe logically, so often our own intuitions are more helpful than computers.</div>
     <div class="landing-point">Simulabra is a tool to model and simulate complex systems of independent agents.</div>
-    <div class="landing-point">It provides a toolbox of methods: agent-based modeling, system dynamics, and discrete event systems.</div>
-    <div class="landing-point">Simple yet powerful programming interface that doesn't require previous experience, as well as integration with Python.</div>
-    <div class="landing-point">Tweak, observe, and analyze your model over the course of your experiments and discover scientific insights.</div>
-    <div class="landing-point">Alternatives all require programming, and typically have confusing interfaces and other barriers to use.</div>
+    <div class="landing-point">It provides a toolbox of methods that help tackle any problem, including agent-based modeling, system dynamics, and discrete event systems.</div>
+    <div class="landing-point">Develop models using a simple yet powerful visual programming interface that doesn't require previous experience, as well as integration with Python for access to its ecosystem of libraries.</div>
+    <div class="landing-point">With it you can tweak, observe, and analyze your model over the course of your experiments and discover scientific insights.</div>
     <div class="landing-point">Interested or just curious? <a href="mailto:contact@simulabra.com">Get in touch.</a></div>
 </div>
