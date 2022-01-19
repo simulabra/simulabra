b7b6460700a306b66a3a3837f00d6cca3a7be741
Riley Stewart <me@rileystew.art>
2022-01-18 22:24:55 -0800

    now on to nom!

--- core/src/lang/parser.rs
+++ core/src/lang/parser.rs
@@ -1,33 +1,3 @@
-pub enum Token {
-    Number(f32),
-    Symbol(String),
-}
-
-peg::parser!{
-    grammar parser() for str {
-        rule number() -> f64
-            = n:$(['0'..='9']+ ("." ['0'..='9']+)?) {? n.parse().or(Err("number")) }
-
-        rule character() -> char
-            = ['a'..='z' | 'A'..='Z']
-
-        rule digit() -> char
-            = ['0'..='9']
-
-        rule alphanumeric() -> char
-            = character() / digit()
-
-        rule symbol() -> str
-            = character
-    }
-}
-
-pub struct Lexer {
-    program: String,
-}
-
-impl Lexer {
-    pub fn tokenize(&self) -> Vec<Token> {
-
-    }
-}
+use nom::{
+    IResult
+};
