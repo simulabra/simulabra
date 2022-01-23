22b5136f0dd3cabb17fb6d7b26c4f275e03bd52c
Riley Stewart <me@rileystew.art>
2022-01-23 09:42:45 -0800

    semantic skelly

--- core/src/agent.rs
+++ core/src/agent.rs
@@ -1,14 +1,32 @@
 use std::fmt::Debug;
 
 #[derive(Debug)]
-pub struct AgentDefinition {
-    pub name: String,
+pub enum SimulabraType {
+    Number,
+    String,
+    Bool,
+    Agent,
 }
 
-impl AgentDefinition {
-    pub fn instantiate(&self) -> Agent {
-        Agent
-    }
+#[derive(Debug)]
+pub struct Identifier(String);
+
+#[derive(Debug)]
+pub struct VarDefinition {
+    identifier: Identifier,
+    ttype: SimulabraType,
+
 }
 
-pub struct Agent;
+#[derive(Debug)]
+pub struct ClassName(String);
+
+
+#[derive(Debug)]
+pub struct ClassDefinition {
+    pub name: ClassName,
+    // whether this can be instantiated as an agent
+    pub agent: bool,
+    // composition over inheritance!
+    pub components: Vec<ClassName>,
+}
