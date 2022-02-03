1d5a90847b0175c979ab3bb926b3ce66ef2835ba
Riley Stewart <me@rileystew.art>
2022-02-02 21:01:59 -0800

    excise python

--- run_sample.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-PIPENV_VENV_IN_PROJECT=1
-pipenv run maturin develop -m simulabra-pylib/Cargo.toml
-cargo build
-pipenv run ./target/debug/simulabra-core models/fire.py
