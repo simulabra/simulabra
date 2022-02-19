8d9c55fdfe4abfe9559dfd5c5238d23679bd7273
Riley Stewart <me@rileystew.art>
2022-02-18 21:56:05 -0800

    start afresh

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use std::path::PathBuf;
-
-use err::SlabResult;
-use clap::Parser;
-use lang::parser::parse;
-pub mod model;
-pub mod agent;
-pub mod err;
-pub mod lang;
-
-#[derive(Parser, Debug)]
-struct SlabArgs {
-    model: Option<PathBuf>,
-}
-fn main() -> SlabResult<()> {
-    parse()
+fn main() {
+    println!("Hello, world!");
 }
