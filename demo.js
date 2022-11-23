c439941f3ad3d08678d0ea3570261885a12e4d29
Riley Stewart <me@rileystew.art>
2022-11-23 15:13:27 -0800

    even less demo

--- demo.js
+++ demo.js
@@ -21,9 +21,10 @@ const _Counter = Base.Class.new({
 const _CallbackCommand = Base.Class.new({
   _name: 'CallbackCommand',
   _slots: {
+    self: Base.Var.new(),
     fn: Base.Var.new(),
     do(...args) {
-      return this.fn().apply(this, args);
+      return this.fn().apply(this.self(), args);
     },
   }
 });
@@ -38,12 +39,12 @@ const _Demo = Base.Class.new({
     })),
     render: Base.Var.default(() => {}),
     load() {
-      const self = this;
       this.counter().load();
       this.button().click(_CallbackCommand.new({
+        _self: this,
         _fn() {
-          self.counter().inc();
-          self.render();
+          this.counter().inc();
+          this.render();
         },
       }));
       this.button().load();
